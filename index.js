586960f74979c6712a7cffcc6a556921f589dca9
shrey_akan <shereyansjain0904@gmail.com>
2024-01-15 16:29:42 +0530

    updated code

--- index.js
+++ index.js
@@ -13,24 +13,33 @@ io.on("connection", (socket) => {
   console.log("connected");
   console.log(socket.id, "has joined");
 
+  // Event: User Signin
   socket.on("signin", (id) => {
     console.log(id);
     clients[id] = socket;
     console.log(clients);
   });
 
+  // Event: User Message
   socket.on("message", (msg) => {
-    try {
-      console.log("Received message:", msg);
-      const targetId = msg.targetId;
-      if (clients[targetId]) clients[targetId].emit("message", msg);
-    } catch (error) {
-      console.error("Error handling message:", error);
-      socket.emit("error", "Error handling the message");
+    console.log("Received message:", msg);
+    const targetId = msg.targetId;
+
+    // Check if the targetId exists and is connected
+    if (clients[targetId]) {
+      clients[targetId].emit("message", msg);
+    } else {
+      console.log("Target user not found or not connected");
+      socket.emit("error", "Target user not found or not connected");
     }
+  });
+
+  // Event: Socket Error
+  socket.on('error', (error) => {
+    console.error('Socket Error:', error);
   });
 });
 
 server.listen(port, "0.0.0.0", () => {
-  console.log("server started");
+  console.log("Server started on port", port);
 });
